75b686fb57be08746883593a029e8dadc0bce56d
ebalp <ebalp@yahoo.com>
2014-09-18 17:41:16 -0500

    Mejoramiento de instrucciones

--- bodega/mysql_connection.R
+++ bodega/mysql_connection.R
@@ -1,9 +1,11 @@
-# http://www.ahschulz.de/2013/07/23/installing-rmysql-under-windows/
-# 1. Instalar mysql-server en local
-# 2. crear variable ambiental en panel de control (MYSQL_HOME)
+# 0. Seguir esto: http://www.ahschulz.de/2013/07/23/installing-rmysql-under-windows/
+# O lo que es igual:
+# 1. Instalar mysql-server en local 
+# 2. crear variable ambiental en panel de control (MYSQL_HOME) el path a mysql server 
 # 3. Instalar Rtools (con extras)
-# 4. Cambiar de lugar: libmysql.dll (de lib a bin)
-# 5. Copiar Rcmd.bat en C:\Program Files\R\R-2.13.2\bin
+# 4. Cambiar de lugar: libmysql.dll (de lib a bin)   en directorio de mysql server
+# 5. Copiar Rcmd.bat en C:\Program Files\R\R-XXXXX\bin
+# 6. Reiniciar compu
 
 install.packages("RMySQL", type = "source")
 library(RMySQL)
